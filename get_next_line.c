3873da542d076ebc92ba1292de1a3939799d0289
Gustavo Serafio Correa De Brito <gserafio@c1r5p3.42sp.org.br>
2024-12-17 23:14:48 -0300

    feat: tentando implementar

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*   By: gserafio <gserafio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 00:18:11 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2024/12/17 16:40:33 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2024/12/17 23:07:35 by gserafio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,52 +22,53 @@
 // o índice da posição do caractere ou um indicador de ausência (-1).
 char *find_newline_in_buffer(char *buffer, int fd)
 {
-	char *tmp;
-//	ssize_t bytes_read;
+	ssize_t bytes_read;
+	char	*buffer_to_concat;
+	char	*tmp;
 
-//	bytes_read = 0;
-	tmp = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!tmp)
+	bytes_read = 0;
+	buffer_to_concat = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer_to_concat)
 		return NULL;
-	ft_strlcpy(tmp, buffer, BUFFER_SIZE + 1);
-	while (ft_strchr(tmp, '\n') == 0)
+	buffer_to_concat[0] = '\0';
+	while (ft_strchr(buffer_to_concat, '\n') == 0)
 	{
-		tmp = malloc(sizeof(char) * BUFFER_SIZE + 1);
-		bytes_read = read(fd, tmp + ft_strlen(tmp), (BUFFER_SIZE - ft_strlen(tmp)));
-		if (!bytes_read)
+		bytes_read = read(fd, buffer_to_concat, BUFFER_SIZE);
+		if (bytes_read == 0)
+			break;
+		buffer_to_concat[bytes_read] = '\0';
+		tmp = buffer;
+		buffer = ft_strjoin(buffer, buffer_to_concat);
+		if (!buffer)
 			return NULL;
 	}
-	printf("%s", buffer);
-	return NULL;
+	return buffer;
 }
 
 char *get_next_line(int fd)
 {
-	static char *buffer;
-	buffer = NULL;
+	static char *buffer[1024];
+	// buffer = NULL;
 
-	if (!buffer)
-	{
-		buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (!buffer)
-			return NULL;
-		buffer = ft_memset(buffer, 0, BUFFER_SIZE);
-		read(fd, buffer, BUFFER_SIZE);
-	}
-	find_newline_in_buffer(buffer);
+	// if (!buffer)
+	// {
+	// 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	// 	if (!buffer)
+	// 		return NULL;
+	// 	buffer = ft_memset(buffer, 0, BUFFER_SIZE);
+	// }
+	find_newline_in_buffer(buffer, fd);
 	return (buffer);
 }
 
 int	main(void)
 {
 	int	fd;
-	char *buffer;
 	fd = open("arquivo.txt", O_RDONLY);
 
 	// int i;
 	// i = 0;
-	buffer = get_next_line(fd);
-	free(buffer);
+	get_next_line(fd);
 	// while (buffer == (get_next_line(fd) != NULL))
 	// {
 	// 	printf("%s", buffer);
